5e4767d8d006b72dfd3e7c46f4954d6d1b135d22
Edjostenes <edjosteneslp@gmail.com>
2021-11-26 12:04:58 -0300

    migrating example to null-safety

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   final String title;
 
-  MyHomePage({@required this.title});
+  MyHomePage({required this.title});
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
