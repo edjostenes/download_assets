51dcc0d3f1fd559e4de29e4458b2d4a90299773b
Edjostenes <edjosteneslp@gmail.com>
2023-01-24 18:04:47 -0300

    removed useless code

--- lib/src/managers/http/custom_http_client_impl.dart
+++ lib/src/managers/http/custom_http_client_impl.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
-import 'package:download_assets/src/managers/http/custom_http_client.dart';
+
+import 'custom_http_client.dart';
 
 class CustomHttpClientImpl implements CustomHttpClient {
   @override
@@ -9,15 +10,14 @@ class CustomHttpClientImpl implements CustomHttpClient {
     String urlPath,
     String savePath, {
     ProgressCallback? onReceiveProgress,
-  }) async {
-    return await Dio().download(
-      urlPath,
-      savePath,
-      options: Options(
-        headers: {HttpHeaders.acceptEncodingHeader: '*'},
-        responseType: ResponseType.bytes,
-      ),
-      onReceiveProgress: onReceiveProgress,
-    );
-  }
+  }) async =>
+      await Dio().download(
+        urlPath,
+        savePath,
+        options: Options(
+          headers: {HttpHeaders.acceptEncodingHeader: '*'},
+          responseType: ResponseType.bytes,
+        ),
+        onReceiveProgress: onReceiveProgress,
+      );
 }
--- test/downloadassets_test.dart
+++ test/downloadassets_test.dart
@@ -1,3 +1,1 @@
-void main() {
-  // TODO: Insert your tests here!
-}
+void main() {}
